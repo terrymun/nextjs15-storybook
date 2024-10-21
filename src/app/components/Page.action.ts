85b07390dee1e9a6b620790ec5e8f60cc2f14fd1
Terry Mun <terry.mun@lego.com>
2024-10-21 23:18:37 +0200

    chore: added comment

--- src/app/components/Page.action.ts
+++ src/app/components/Page.action.ts
@@ -3,8 +3,11 @@
 import { headers } from "next/headers";
 
 export const dummyServerAction = async () => {
+  // NOTE: This is the line that wiill causes Storybook to fail
+  //       If you comment it out and replace with the line below, Storybook will work:
+  //       const referrer = 'http://localhost:6006/';
   const referrer = (await headers()).get('referer');
-  
+
   return {
     message: 'Server action was successful',
     referrer,
